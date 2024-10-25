404ec8ccf960e380fce0e007fc6f66b9a292031e
Danilok95432 <danila.dvoretskov@yandex.ru>
2024-10-25 14:48:16 +0300

    Сделано перенаправление на /posts по умолчанию

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import PostPage from "./pages/PostPage";
 import PostsPage from "./pages/PostsPage";
 import UserPage from "./pages/UserPage";
-import MainPage from "./pages/MainPage";
 import AuthorizationPage from "./pages/AuthorizationPage";
 import Header from "./components/Header";
 
@@ -16,7 +15,7 @@ function App() {
         <Route path="/posts/:id" element={<PostPage />} />
         <Route path="/posts" element={<PostsPage />} />
         <Route path="/authorization" element={<AuthorizationPage />} />
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<Navigate to="/posts" replace />} />
       </Routes>
     </Router>
   );
